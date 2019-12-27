72ba66e006e04fc7de4c599ee22a9515d9babee3
yev <yevhenii.pykhov@nure.ua>
2019-12-27 13:26:13 +0200

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,7 @@
+import './styles/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    context: path.resolve('src'),
+    entry: {
+        app: './app',
+    },
+    output: {
+        path: path.resolve('docs'),
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*',
+                '!.gitkeep',
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react',
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+};
